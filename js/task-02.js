615e8c2f1ed2577b6fb376ba2140a936c3364158
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-03-24 20:44:36 +0200

    task 02 first solution

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newElements = [];
+
+for (const ingredient of ingredients) {
+  const newEl = document.createElement('li');
+  newEl.textContent = ingredient;
+  newEl.classList.add('item');
+  newElements.push(newEl);
+}
+console.log(newElements);
+document.querySelector('#ingredients').append(...newElements);
