d68f466a4fd6d96eb314f489bbfaff3302262e51
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-03-26 15:23:25 +0200

    task 08 first solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('form.login-form');
 
+formEl.addEventListener('submit', submitForm);
+
+function submitForm(event){
+    event.preventDefault();
+    const {
+        elements : {email, password}
+    } = event.currentTarget;
+
+    if (email.value.length === 0 || password.value.length === 0) {
+        alert('Please, fill in all the fields.');
+        return;
+    }
+
+    const submitObject = {};
+    
+    submitObject[email.name] = email.value;
+    submitObject[password.name] = password.value;
+
+    this.reset();
+
+    console.log(submitObject);
+}
