3946e32733eb129e438be2bd0421bdfdeea21c88
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-03-27 19:08:41 +0300

    task 08 first solution

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const ref = {
+  changeColorBtn : document.querySelector('button.change-color'),
+  colorSpan : document.querySelector('span.color'),
+}
+
+ref.changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  const colorHex = getRandomHexColor();
+  document.body.style.backgroundColor = colorHex;
+  ref.colorSpan.innerText = colorHex.toString();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
